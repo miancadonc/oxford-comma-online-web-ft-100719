451b8c66804b169b4afd04d844fc12b95ce19424
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 23:00:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  case length
+  case array.size
+  when 1
+    array.join
+  when
+      
   final_word = array.pop
   final_string = array.join(", ")
   final_string + ", and " + final_word
