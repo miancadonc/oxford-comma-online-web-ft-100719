55dbcecd97dbc96f9dfdcbbae26d7fcabec5cc14
Nat McConnaughay <natmcconnaughay@gmail.com>
2019-10-12 23:04:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,9 @@
 def oxford_comma(array)
-  case array.size
-  when 1
+  if array.size == 1
     final_string = array.join
-  when 2
+  elsif array.size == 2 
     final_string = array.join(" and ")
-  when >2
+  else
     final_word = array.pop
     string = array.join(", ")
     final_string = string + ", and " + final_word
